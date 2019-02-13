8c756f15799e7f5e4865c70f33346ceacf3e5a23
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-13 15:48:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,8 +8,8 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 199
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Strider", "Not all who wander are lost.", "human", 2, 2), (6, "Frodo Baggins", "I will do it.", "hobbit", 2, 2), (7, "Gandalf", "You shall not pass!", "wizard", 2, 2), (8, "Boromir", "One does not simply walk into Mordor.", "human", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Strider", "Not all who wander are lost.", "human", 2, 2), (6, "Bilbo Baggins", "The road goes ever on and on.", "hobbit", 2, 2), (7, "Gandalf", "You shall not pass!", "wizard", 2, 2), (8, "Boromir", "One does not simply walk into Mordor.", "human", 2, 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
-INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 5, 5), (11, 6, 5) (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 5, 5), (11, 6, 5), (12, 4, 6), (13, 6, 6), (14, 6, 6) (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
