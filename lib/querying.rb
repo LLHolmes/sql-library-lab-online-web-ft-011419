96dfacfd33f84b4780911e2c21fc9b7db275f15c
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-13 17:40:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT character.species, COUNT(character_species)
+  FROM characters;"
 end
 
 def select_name_and_series_subgenres_of_authors
