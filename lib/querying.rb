1e3250610e8b117f19d7f9c996313251a99db0fa
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-13 17:37:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ def select_name_and_motto_of_char_with_longest_motto
   # "SELECT characters.name, MIN(characters.motto) FROM characters;"
   "SELECT characters.name, characters.motto 
   FROM characters
-  GROUP BY LENGTH(characters.motto) DESC 
+  ORDER BY LENGTH(characters.motto) DESC 
   LIMIT 1;"
 end
 
