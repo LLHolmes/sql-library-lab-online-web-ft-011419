d4bace608919ad8e83fcd09b3b17251549c11d28
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-13 17:20:09 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,9 +1,1 @@
--- CREATE TABLE characters (
---   id INTEGER PRIMARY KEY,
---   name TEXT,
---   motto TEXT,
---   species TEXT,
---   author_id INTEGER,
---   series_id INTEGER
--- );
 UPDATE characters SET species = "Martian" WHERE id = 8;
