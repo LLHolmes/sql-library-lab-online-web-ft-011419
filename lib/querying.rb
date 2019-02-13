ff9e5c0dd3dc7bc262eba384da212d3bc7a7d03f
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-13 17:44:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species)
+  "SELECT characters.species, COUNT(*)
   FROM characters
   GROUP BY characters.species
   ORDER BY COUNT(*) DESC
