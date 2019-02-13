d207113d4903db22f03f72bf4c2665e4586b3231
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-13 17:53:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   JOIN characters ON series.author_id = characters.author_id
-  GROUP BY ;"
+  WHERE characters.species = 'human'
+  GROUP BY;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
